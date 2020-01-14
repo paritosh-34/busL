1dfedfdadac48f1d946e79195bddc1622f5adacc
Paritosh Batish <batishparitosh2@gmail.com>
2020-01-14 13:18:31 +0530

    Location updater 1.0

--- piLocationUpdate.py
+++ piLocationUpdate.py
@@ -3,13 +3,15 @@ import json
 import time
 
 my_url = "http://127.0.0.1:5000/api/latlon"
+# Change the url respectively
 
 while True:
+    # This is the format to send lat lon
     data = {
-        "lat": "1.1.1.1",
-        "lon": "2.2.2.2"
+        "lat": "1.1.1.1",  # lat here
+        "lon": "2.2.2.2",  # lon here
     }
     y = json.dumps(data)
     r = requests.post(url=my_url, json=data)
-    # time.sleep(6)
+    time.sleep(6)
     break
