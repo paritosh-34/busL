c93c6ea01bc1890b153b9fef266c09871ebfe962
Paritosh Batish <batishparitosh2@gmail.com>
2020-01-22 21:57:42 +0530

    Modified for request testing

--- piLocationUpdate.py
+++ piLocationUpdate.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 
-my_url = "http://127.0.0.1:5000/api/latlon"
+my_url = "http://127.0.0.1:5000/post?lat=1.2.3.4&lon=1.2.3.4"
 # Change the url respectively
 
 while True:
@@ -12,6 +12,7 @@ while True:
         "lon": "2.2.2.2",  # lon here
     }
     y = json.dumps(data)
-    r = requests.post(url=my_url, json=data)
-    time.sleep(6)
+    r = requests.post(url=my_url)
+    print(r)
+    # time.sleep(6)
     break
