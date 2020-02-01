1aee368ac42f0711b31967661e532a94802b781e
Paritosh Batish <54840316+paritosh-34@users.noreply.github.com>
2020-02-01 11:40:09 +0530

    only post method

--- main_0.5.py
+++ main_0.5.py
@@ -51,7 +51,7 @@ def test_api():
 
 @app.route('/post', methods=['GET', 'POST'])
 def latlon():
-    if request.method == 'POST:
+    if request.method == 'POST':
         lat = request.args.get('lat')
         lon = request.args.get('lon')
         print(lat)
