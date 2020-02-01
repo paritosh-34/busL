2a2d69d9b32e6b71625e92fe8ec6b0dcc50e607f
Paritosh Batish <batishparitosh2@gmail.com>
2020-02-01 17:42:01 +0530

    return boolean

--- main_0.5.py
+++ main_0.5.py
@@ -27,10 +27,10 @@ def home():
         re = mongo.db.users.find_one({"name": username, "pass": userpass})
         print(re)
         if re is None:
-            return render_template('login.html', i="invalid Id/Password")
+            return False
         else:
             session['user'] = username
-            return redirect('/')
+            return True
 
     return render_template('login.html')
 
